b58a5e5bf759f42b8c6125bc75043be4479add70
Rubens Galarraga <rgalarraga121@gmail.com>
2023-01-10 09:52:05 -0300

    add

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input}
+         placeholder="add a new task"
+         autoComplete="off"
+         autoCorrect={false}
+         autoCapitalize= 'none'
+         
+         />
+        <Button title="ADD" color="#626893" />
+        </View>
     </View>
   );
 }
@@ -13,8 +22,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    justifyContent: "space-between",
+    flexDirection: 'row',
+    marginTop: 50,
+    marginHorizontal: 20,
+  },
+  input: {
+    width: '75%',
+    borderBottomColor: "#626893",
+    borderBottomWidth: 1,
+    height: 40,
+    color:'#212121'
   },
 });
